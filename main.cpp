86047bacca5cf6803ff82804e45e5e1e834bddf6
lbertrand417 <81630925+lbertrand417@users.noreply.github.com>
2021-11-23 14:03:16 +0100

    nothing

--- main.cpp
+++ main.cpp
@@ -39,7 +39,15 @@ int main(int argc, char *argv[])
     compute_edges_weight(mesh.V, mesh.F);
     // Compute Laplacian matrix
     compute_laplacian_matrix();*/
-    MatrixXd new_V = mesh.V;
+    MatrixXd R(3, 3);
+    R << cos(3.14 / 2), 0, 0,
+        0, sin(3.14 / 2), 0,
+        0, 0, 1;
+    MatrixXd new_V(mesh.V.rows(), mesh.V.cols());
+    for (int i = 0; i < mesh.V.rows(); i++) {
+        new_V.row(i) = mesh.V.row(i) * R.transpose();
+    }
+    std::cout << new_V << std::endl;
     std::list<std::pair<int, Vector3d>> C;
     arap(mesh.V, mesh.F, C, new_V);
 
@@ -67,7 +75,8 @@ int main(int argc, char *argv[])
     };
 
     // Plot the mesh
-    viewer.data().set_mesh(mesh.V, mesh.F);
+    //viewer.data().set_mesh(mesh.V, mesh.F);
+    viewer.data().set_mesh(new_V, mesh.F);
     viewer.data().set_face_based(true);
     viewer.launch();
 }
